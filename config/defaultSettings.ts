8b3fe7bcab5759fdff561d3754e6f85b58995aef
fangchaoduan <namefangchaoduan@163.com>
2023-10-20 19:20:32 +0800

    feat: 配置了框架的全局默认配置；

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -1,27 +1,51 @@
-import { ProLayoutProps } from '@ant-design/pro-components';
+import { MenuDataItem, ProLayoutProps } from '@ant-design/pro-components';
 
 /**
- * @name
+ * @name 框架默认的配置; 可通过ProLayoutSettingsType查看可以配置什么,以及ProLayoutProps来查看多余的antd-pro提供的功能;
  */
-const Settings: ProLayoutProps & {
+interface ProLayoutSettingsType extends ProLayoutProps {
   pwa?: boolean;
   logo?: string;
-} = {
-  navTheme: 'light',
-  // 拂晓蓝
-  colorPrimary: '#1890ff',
-  layout: 'mix',
-  contentWidth: 'Fluid',
-  fixedHeader: false,
-  fixSiderbar: true,
-  colorWeak: false,
-  title: 'Ant Design Pro',
+}
+const Settings: ProLayoutSettingsType = {
+  navTheme: 'light', //导航的主题;
+
+  colorPrimary: '#1890ff', //拂晓蓝;//主色，需要配合umi使用;
+  layout: 'mix', //layout的布局方式;//'侧边菜单side' | '顶部菜单top' | '混合布局mix-既有顶部也有侧边';
+  contentWidth: 'Fluid', //内容布局:'Fluid流动' | 'Fixed固定'，仅在布局为顶部时有效;
+  fixedHeader: false, //是否固定头部;
+  fixSiderbar: true, //是否固定侧边栏;
+  colorWeak: false, //是否全局增加滤镜;
+  pure: false, //是否简约模式,设置之后不再渲染任何layout的东西，但是会有 context，可以获取到当前菜单。
+  title: '框架Layout的标题', //Layout的title，也会显示在浏览器标签上;
+  // 同步处理menuData的数据，可以动态的控制数据; 异步数据推荐使用 menu.request 和 params。
+  menuDataRender: (theList: MenuDataItem[]) => {
+    let theMenuItem: MenuDataItem = {
+      path: '/一级测试路由URL路径',
+      name: '一级测试路由',
+      icon: 'smile',
+    };
+    let theMenuList = (theList || []).concat(theMenuItem);
+    return theMenuList;
+  },
   pwa: true,
-  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
-  iconfontUrl: '',
+  logo: 'https://www.baidu.com/img/PCfb_5bf082d29588c07f842ccde3f97243ea.png', //首页logo的配置，可以配置url，React组件和false;
+  iconfontUrl: '', //菜单可以添加自己IconFont的项目图标,应该可以用相对路径也可用绝对路径;
+
+  // 参见ts声明，demo 见文档，通过token 修改样式。Token 是一种设计系统的基本元素，可以使用 Token 快速的修改组件库的基础样式。Layout 中可以通过 token 属性来配置这些颜色。
+  //https://procomponents.ant.design/components/layout#通过-token-修改样式；
+  // [设计系统的基本元素Token]( https://procomponents.ant.design/components/layout#token )
+  // bgLayout: 'linear-gradient(red, yellow 28%)',
   token: {
-    // 参见ts声明，demo 见文档，通过token 修改样式
-    //https://procomponents.ant.design/components/layout#%E9%80%9A%E8%BF%87-token-%E4%BF%AE%E6%94%B9%E6%A0%B7%E5%BC%8F
+    bgLayout: 'transparent', //layout的背景颜色；
+    //侧边side的 token 配置;
+    sider: {
+      colorMenuBackground: 'linear-gradient(to bottom, #1D36AA, #1667DB, #00D3A1)', //menu的背景颜色;
+      colorTextMenuTitle: '#fff', //sider的标题字体颜色;
+      colorTextMenu: '#fff', //menuItem的字体颜色;
+      colorTextMenuSelected: '#fff', //menuItem的选中字体颜色;
+      colorTextMenuItemHover: '#fff', //menuItem的hover字体颜色;
+    },
   },
 };
 
