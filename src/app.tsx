6aaed09327f27eee09e2fd281853f75f51fdc9ef
fangchaoduan <namefangchaoduan@163.com>
2023-12-11 15:43:39 +0800

    feat: 配置浏览器网页标签上的ico网页图标;

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,15 @@
 import Footer from '@/components/Footer';
 import { Question, SelectLang } from '@/components/RightContent';
 import { LinkOutlined } from '@ant-design/icons';
-import type { Settings as LayoutSettings } from '@ant-design/pro-components';
-import { SettingDrawer } from '@ant-design/pro-components';
+import { SettingDrawer, Settings as LayoutSettings } from '@ant-design/pro-components';
 import type { RunTimeLayoutConfig } from '@umijs/max';
 import { history, Link } from '@umijs/max';
 import defaultSettings from '../config/defaultSettings';
 import { AvatarDropdown, AvatarName } from './components/RightContent/AvatarDropdown';
 import { errorConfig } from './requestErrorConfig';
 import { currentUser as queryCurrentUser } from './services/ant-design-pro/api';
-const isDev = process.env.NODE_ENV === 'development';
-const loginPath = '/user/login';
+const isDev = process.env.NODE_ENV === 'development'; //是否是开发模式;
+const loginPath = '/user/login'; //登录页的路由地址;
 
 // 配置所有无需登录的路径;
 const noLoginRequiredPathList = [`/noLoginRequiredLayoutPage`, `/noLoginRequiredPage`];
